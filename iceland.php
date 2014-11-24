8a5275d686edc71d6dbfae49ade07d9ea8ec8c10
jesmaail <josephesmaail93@gmail.com>
2014-11-24 20:21:28 +0000

    Tidied up database connect code.

--- iceland.php
+++ iceland.php
@@ -7,21 +7,15 @@
 		$user = "root";
 		$password = "";
 		$db = "cw2";
-
 		$conn = mysql_connect($server, $user, $password);
 		$db_select_success = mysql_select_db($db,$conn);
 
-		if(!$conn)
-		{
-			die("Error connecting to MySQL: ");
+		if(!$conn){ 
+			die("Error connecting to MySQL: ". mysql_error());	
 		}
-
-		if(!$db_select_success)
-		{
-			die("Error selecting database: ");
-		}
-		else
-		{
+		if(!$db_select_success){
+			die("Error selecting database: ". mysql_error());
+		}else{
 			echo "MySQL database: " . $db ." selected.";
 		}
 	?>
