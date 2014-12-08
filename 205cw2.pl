f230ac0091b0fab242961a49569385d99ea21c6c
jesmaail <josephesmaail93@gmail.com>
2014-12-08 22:52:52 +0000

    Completed questions 1a and 1b

--- 205cw2.pl
+++ 205cw2.pl
@@ -1,8 +1,130 @@
 %Joseph Esmaail - 713992
 %Christopher Lacey - 
-%Patrick Slugbummersucker - 
+%Patrick Schlumberger-Socha - 
 
-%Question 1
+%Question 1(a) - [5 marks]
 
 %Program: ROYAL
 parent(queenmother, elisabeth).
+parent(elisabeth, charles).
+parent(elisabeth, andrew).
+parent(elisabeth, anne).
+parent(elisabeth, edward).
+parent(diana, william).
+parent(diana, harry).
+parent(sarah, beatrice).
+parent(anne, peter).
+parent(anne, zara).
+parent(george, elisabeth).
+parent(philip, charles).
+parent(philip, andrew).
+parent(philip, edward).
+parent(charles, william).
+parent(charles, harry).
+parent(andrew, beatrice).
+parent(andrew, eugene).
+parent(mark, peter).
+parent(mark, zara).
+parent(william, georgejun).
+parent(kate, georgejun).
+
+%1
+%Swear there is a cleaner way to do this and the males!
+the_royal_females(X) :-
+					X==queenmother;
+					X==elisabeth;
+					X==anne;
+					X==diana;
+					X==sarah;
+					X==beatrice;
+					X==zara;
+					X==kate.
+%2
+the_royal_males(X) :-
+					X==charles;
+					X==andrew;
+					X==edward;
+					X==william;
+					X==harry;
+					X==peter;
+					X==george;
+					X==phillip;
+					X==eugene;
+					X==mark;
+					X==georgejun.
+
+%3
+the_royal_family(X) :- 
+				the_royal_males(X);
+				the_royal_females(X).
+
+%4
+mother(X,Y) :- 
+			parent(X,Y),
+			the_royal_females(X).
+
+%5
+has_child(X) :- parent(X,_).
+
+%6
+ancestor(X,Y) :- parent(X,Y).
+ancestor(X,Y) :- 
+				parent(X,Z),
+				ancestor(Z,Y).
+
+%7
+descendant(X,Y) :- ancestor(Y,X).
+
+%--Translate the following into queries--
+%8 Who is the Mother of Beatrice?
+	%mother(A, beatrice).
+	%A = sarah.
+
+%9 Who has a child (one or more)?
+	%has_child(A).
+	%A = queenmother;
+	%A = elisabeth;
+	%A = diana;
+	%A = sarah;
+	%A = anne;
+	%A = george;
+	%A = philip;
+	%A = charles;
+	%A = andrew;
+	%A = mark;
+	%A = william;
+	%A = kate.
+	%^note:duplicates ommitted.
+
+%10 Who is a descendant of the Queenmother?
+	%descendant(A, queenmother).
+	%A = elisabeth;
+	%A = charles;
+	%A = andrew;
+	%A = anne;
+	%A = edward;
+	%A = william;
+	%A = harry;
+	%A = georgejun;
+	%A = beatrice;
+	%A = eugene;
+	%A = peter;
+	%A = zara.
+
+
+%------------------------------------------------------------
+%Question 1(b) - [5 marks]
+%Use predicates of Q1a to define predicates sibling/2 and aunt/2
+%Query: Who are the siblings of charles?
+
+sibling(X,Y) :- 
+			parent(A,X),
+			parent(A,Y).
+
+aunt(X,Y) :- 
+			parent(Z,Y),
+			sibling(X,Z),
+			the_royal_females(X).
+
+%------------------------------------------------------------
+%Question 1(c) - [5 marks]
