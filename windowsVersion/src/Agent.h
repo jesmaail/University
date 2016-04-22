e1dcc94e952ea10ec08b6b102056975b0f3d5a94
jesmaail <josephesmaail93@gmail.com>
2016-04-22 21:33:53 +0100

    Solving NN issues when connected to ALE

---
+++ windowsVersion/src/Agent.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "network/NeuralNetwork.h"
+#include "network/ReplayMem.h"
+#include <ale_interface.hpp>
+#include <vector>
+
+class Agent{
+private:
+	const int REPLAY_MEM_SIZE = 10000;
+	const double LEARNING_RATE = 0.99;
+	const int EPOCH_COUNT = 5;
+	ALEInterface m_ale;
+
+	int m_numActions;
+	int m_epsilon = 99;
+	double discount = 0.99;
+	Images m_current;
+	Images m_next;
+	int m_reward;
+	int m_action;
+	bool m_terminal = false;
+	ReplayMem m_replay;
+
+	weightStruct m_weights[2];
+
+
+public:
+	Agent(string game);
+
+	void play();
+
+	Images Preprocess(Images in);
+
+	void PerformRandomAction();
+
+	void UseNeuralNetwork(/*weights?*/);
+
+	void Backprop(); //should this be here or in NN?
+
+	weightStruct InitRandWeights();
+
+	Images GetPlaceholderScreen();
+
+	void Learning();
+};
