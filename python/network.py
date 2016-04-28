563f1da10e102772c06139dfc9d4efbd93641a76
Joseph Esmaail <josephesmaail93@gmail.com>
2016-04-28 20:00:34 +0100

    Working on python

--- python/network.py
+++ python/network.py
@@ -5,8 +5,11 @@ import sys
 import math
 from random import randrange
 from ale_python_interface import ALEInterface
+from collections import deque
 
 USE_SDL = True
+DISP_SCREEN = False
+PLAY_SOUND = False
 
 EPOCH_COUNT = 1
 EPISODE_COUNT = 1
@@ -18,7 +21,6 @@ INPUT_Y = 210
 DESIRED_XY = 84
 ROI_TOP = 32
 ROI_BOT = 18
-
 
 FIRST_F_SIZE = 8
 FIRST_F_COUNT = 2
@@ -39,68 +41,28 @@ SECOND_L_SIZE = 20
 THIRD_L_SIZE = 9
 FOURTH_L_SIZE = 256
 
-def main():
-	if len(sys.argv) < 2:
-  		print 'Usage:', sys.argv[0], 'rom_file'
-  		sys.exit()
 
-  	global ale
-	ale = ALEInterface()
-	ale.setInt('random_seed', 123)
+def conv_layer(input, filter, stride):
+	return tf.nn.relu(tf.nn.conv2d(input, filter, [1, stride, stride, 1], "SAME"))
 
-	if USE_SDL:
-		ale.setBool('sound', False)
-		ale.setBool('display_screen', False)
-
-	ale.loadROM(sys.argv[1])
-
-	global legal_actions, action_size
-	legal_actions = ale.getLegalActionSet()
-	action_size = len(legal_actions)
-
-	for i in range(EPISODE_COUNT):
-		agent()
+def full_conn_layer(input, filter):
+	return tf.nn.relu(tf.matmul(input, filter))
 
 
-def weight_init(struct):
-	structure = tf.truncated_normal(struct, stddev = 0.01)
-	return tf.Variable(structure)
-
-
-def create_conv_layer(input, filter, stride):
-	strides = [1, stride, stride, 1]
-	return tf.nn.conv2d(input, filter, strides, "SAME")
+def init_network():
+	#weights = init_weights()
+	ph_in = tf.placeholder(tf.float32, [None, 84, 84, 1]) #84*84
+	second_layer = conv_layer(ph_in, weights['w1'], FIRST_F_STRIDE)
+	third_layer = conv_layer(second_layer, weights['w2'], SECOND_F_STRIDE)
+	third_reshape = tf.reshape(third_layer, [-1, 484])
+	fourth_layer = full_conn_layer(third_reshape, weights['w3'])
+	output_layer = tf.matmul(fourth_layer, weights['w4'])
+	return ph_in, output_layer
 
 
 def perform_random_action():
 	return randrange(action_size)
 
-
-def create_neural_network():
-	input_layer = tf.placeholder("float", [None, 84, 84, 1])
-	# X = tf.reshape(game_screens, shape=[-1, 84, 84, 1])
-	# print X
-
-	first_weights = weight_init(FIRST_FILTER)
-	second_layer = tf.nn.relu(create_conv_layer(input_layer, first_weights, FIRST_F_STRIDE))
-	# second_layer = tf.nn.relu(create_conv_layer(X, first_weights, FIRST_F_STRIDE))
-	# print second_layer
-
-	second_weights = weight_init(SECOND_FILTER)
-	third_layer = tf.nn.relu(create_conv_layer(second_layer, second_weights, SECOND_F_STRIDE))
-	# print third_layer
-
-	reshape_third = tf.reshape(third_layer, [-1, 484])
-
-	third_weights = weight_init(THIRD_FILTER)
-	fourth_layer = tf.nn.relu(tf.matmul(reshape_third, third_weights))
-	# print fourth_layer
-
-	fourth_weights = weight_init([256, action_size])
-	output_layer = tf.nn.relu(tf.matmul(fourth_layer, fourth_weights))
-	# print output_layer
-
-	return np.argmax(output_layer)
 
 def get_screen():
 	raw_screen = ale.getScreenGrayscale()
@@ -110,45 +72,84 @@ def get_screen():
 	return scale_screen.astype("float32")
 
 
-def agent():
-	#create minibatch
-	# screens = []
-	global action
-	action = 0
-	screen = get_screen()
-	# screens.append(screen)
 
-	for i in range(EPOCH_COUNT):
+if len(sys.argv) < 2:
+	print 'Usage:', sys.argv[0], 'rom_file'
+	sys.exit()
 
-		while ale.game_over() == False:
-			if randrange(100)+1 < EPSILON:
-				action = perform_random_action()
-				print "random ", action
-			else:
-				action = create_neural_network(screen)
-				print "network ", action
+global ale
+ale = ALEInterface()
+ale.setInt('random_seed', 123)
 
-			reward = ale.act(legal_actions[action])
-			#get next screen
+if USE_SDL:
+	ale.setBool('sound', PLAY_SOUND)
+	ale.setBool('display_screen', DISP_SCREEN)
 
+ale.loadROM(sys.argv[1])
 
+global legal_actions, action_size
+legal_actions = ale.getLegalActionSet()
+action_size = len(legal_actions)
 
-			if ale.game_over():
-				print "game over"
-			#if gameover:
-				#terminal flag for transiton
+global replay_mem
+replay_mem = deque()
 
-			#add transition to replay memory
-
-			#perform learning
+weights ={
+	'w1' : tf.Variable(tf.random_normal(FIRST_FILTER)),
+	'w2' : tf.Variable(tf.random_normal(SECOND_FILTER)),
+	'w3' : tf.Variable(tf.random_normal(THIRD_FILTER)),
+	'w4' : tf.Variable(tf.random_normal([256, action_size]))
+}
 
 
+sess = tf.InteractiveSession()
+net_in, net_out = init_network()
 
-	# values =[1,2,3,4,5,6,7,8,9,10]
-	# for value in values:
-	# 	print(value)
+ph_out = tf.placeholder(tf.float32, [None, action_size])
+ph_target = tf.placeholder(tf.float32, [None])
 
-	# values1 = [x*2 for x in range(10)]
+action_choice = tf.reduce_sum(tf.mul(net_out, ph_out), reduction_indices = 1)
+cost = tf.reduce_mean(tf.square(ph_target - action_choice))
+optimizer = tf.train.RMSPropOptimizer(0.99).minimize(cost)
+
+# init = tf.initialise_all_variables()
 
 
-main()
+global action
+action = 0
+screen = get_screen()
+screen = np.reshape(screen, (1, 84, 84))
+ale.act(legal_actions[0])
+screen2 = get_screen()
+screen2 = np.reshape(screen2, (1, 84, 84))
+
+# state = [screen, screeneen2]
+state = [[[]]]
+state.append(screen)
+
+
+
+for i in range(EPOCH_COUNT):
+
+	#input_img = tf.convert_to_tensor(screens)
+
+	net_result = net_out.eval(feed_dict={net_in : screen})
+
+	while ale.game_over() == False:
+		if randrange(100)+1 < EPSILON:
+			action = perform_random_action()
+			#print "r:", action
+		else:
+			#net_out = run_network(screen, weights)
+			#action = np.argmax(net_out)
+			print "n:", action
+
+		reward = ale.act(legal_actions[action])
+
+		next_screen = get_screen()
+		terminal = ale.game_over()
+
+		if ale.game_over():
+			print "game over"
+
+		replay_mem.append((screen, action, reward, next_screen, terminal))
