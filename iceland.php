jesmaail/250PHP

added top table styling

iceland.php
@@ -5,7 +5,7 @@
 	<?php
 		$server = "localhost";
 		$user = "root";
-		$password = "";
+		$password = "sabih93";
 		$db = "cw2";
 		$conn = mysql_connect($server, $user, $password);
 		$db_select_success = mysql_select_db($db,$conn);
@@ -16,8 +16,63 @@
 		if(!$db_select_success){
 			die("Error selecting database: ". mysql_error());
 		}else{
-			echo "MySQL database: " . $db ." selected.";
+			// echo "MySQL database: " . $db ." selected.";
 		}
 	?>
+
+	<div id="header">
+		<h1> Order Acknowledgement </h1>
+		<img src = "logo.svg" style="float:right"/>
+	</div>
+	<hr/>
+	<div id="top">
+
+		<div id="deliverydetails" style="float:left">			
+			<table> 
+				<b>Delivery Details</b>
+				<tr>
+					<td>Order #: </td>
+					<td><?php 	?></td>
+				</tr>
+				<tr>
+					<td>Name: </td>
+					<td><?php 	?></td>
+				</tr>
+				<tr>
+					<td>Email: </td>
+					<td><?php 	?></td>
+				</tr>
+				<tr>
+					<td>Address: </td>
+					<td><?php 	?></td>
+				</tr>
+			</table>
+		</div>
+
+		<div id="paymentdetails" style="float:right">
+			<table >
+				<b>Payment Details</b>
+				<tr>
+					<td>Total #: </td>
+					<td><?php 	?></td>
+				</tr>
+				<tr>
+					<td>Method: </td>
+					<td><?php 	?></td>
+				</tr>
+				<tr>
+					<td>Card: </td>
+					<td><?php 	?></td>
+				</tr>
+				<tr>
+					<td>Date: </td>
+					<td><?php 	?></td>
+				</tr>
+			</table>
+
+		</div>
+
+	</div>
+
 	</body>
 </html>

jesmaail/University

iceland.php
@@ -5,7 +5,7 @@
 	<?php
 		$server = "localhost";
 		$user = "root";
-		$password = "";
+		$password = "password";
 		$db = "cw2";
 		$conn = mysql_connect($server, $user, $password);
 		$db_select_success = mysql_select_db($db,$conn);
@@ -16,8 +16,63 @@
 		if(!$db_select_success){
 			die("Error selecting database: ". mysql_error());
 		}else{
-			echo "MySQL database: " . $db ." selected.";
+			// echo "MySQL database: " . $db ." selected.";
 		}
 	?>
+
+	<div id="header">
+		<h1> Order Acknowledgement </h1>
+		<img src = "logo.svg" style="float:right"/>
+	</div>
+	<hr/>
+	<div id="top">
+
+		<div id="deliverydetails" style="float:left">			
+			<table> 
+				<b>Delivery Details</b>
+				<tr>
+					<td>Order #: </td>
+					<td><?php 	?></td>
+				</tr>
+				<tr>
+					<td>Name: </td>
+					<td><?php 	?></td>
+				</tr>
+				<tr>
+					<td>Email: </td>
+					<td><?php 	?></td>
+				</tr>
+				<tr>
+					<td>Address: </td>
+					<td><?php 	?></td>
+				</tr>
+			</table>
+		</div>
+
+		<div id="paymentdetails" style="float:right">
+			<table >
+				<b>Payment Details</b>
+				<tr>
+					<td>Total #: </td>
+					<td><?php 	?></td>
+				</tr>
+				<tr>
+					<td>Method: </td>
+					<td><?php 	?></td>
+				</tr>
+				<tr>
+					<td>Card: </td>
+					<td><?php 	?></td>
+				</tr>
+				<tr>
+					<td>Date: </td>
+					<td><?php 	?></td>
+				</tr>
+			</table>
+
+		</div>
+
+	</div>
+
 	</body>
 </html>